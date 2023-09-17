446c5f327311780b165662b23ffd897385c4effb
Aayush Shukla <aayush.shukla23@gmail.com>
2023-09-17 08:50:41 +0530

    Two Sum Sub3

--- two_sum.cpp
+++ two_sum.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
+        int a,b;
         for(int i=0;i<nums.size();i++){
             for(int j=0;j<nums.size();j++){
                 if((nums.at(i)+nums.at(j))==target && i!=j){
-                    result.push_back(i);
-                    result.push_back(j);
-                    return result;
+                   
+                    a=i;
+                    b=j;
+                    break;
                 }
+
             }
         }
+       result.push_back(a);
+       result.push_back(b);  
       return result;
     }
 };
